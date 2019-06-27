99791b6f1c1be7b54c1c3db38975b62b1f9fb674
Senthil <senthil>
2019-06-26 22:11:41 -0400

    first commit

---
+++ src/main/java/com/senthil/maven/buildnumber/BuildnumberApplication.java
@@ -0,0 +1,13 @@
+package com.senthil.maven.buildnumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildnumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildnumberApplication.class, args);
+	}
+
+}
